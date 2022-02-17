5e8df4bf73a280dc0eeb957dacd6e434785cf572
hellraiser125 <75289151+hellraiser125@users.noreply.github.com>
2022-02-17 18:54:04 +0200

    ...

--- 17.02/task1.py
+++
@@ -1,3 +0,0 @@
-mylist = [4, -9, 8, -11, 8]
-print(list(filter(lambda x : x < 0, mylist)))
-
--- 17.02/task2.py
+++
@@ -1,4 +0,0 @@
-players = ['Ashleigh Barty', 'Simona Halep', 'Naomi Osaka', 'Karolina Pliskova', 'Elina Svitolina']
-players[0], players[-1] = players[-1], players[0]
-print(players)
-
